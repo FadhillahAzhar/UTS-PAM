e64dc26dd91c62b7db84f2b92f1aff51db56dc79
FadhillahAzhar <fadhil.rf08@gmail.com>
2022-03-27 07:14:08 +0700

    apdet lagi

--- uts/component/Feed.js
+++ uts/component/Feed.js
@@ -3,23 +3,24 @@ import { View, Text, StyleSheet, Colors, TextInput, TouchableOpacity, Pressable,
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { Picker } from "@react-native-picker/picker";
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { Jadwal } from './db/Database';
 
 const Stack = createNativeStackNavigator();
 
 const Form = ({navigation}) => {
     const [text, onChangeText] = useState({
-        asal: '',
-        tujuan: '',
-        layanan: '',
-        tanggal: '',
-        jam: '',
+        asal: 'Jawa',
+        tujuan: 'Sumatera',
+        layanan: 'Ekonomi',
+        // tanggal: '',
+        // jam: '',
     });
 
-    const handleText = (textInput) => {
-        return (value) => {
-            onChangeText({ ...text, [textInput]: value });
-        }
-    }
+    // const handleText = (textInput) => {
+    //     return (value) => {
+    //         onChangeText({ ...text, [textInput]: value });
+    //     }
+    // }
     
     return (
         <View style={Style.container}>
@@ -77,7 +78,7 @@ const Form = ({navigation}) => {
                             style={{width: 280 }}
                             onValueChange={(itemValue) => onChangeText({...text, layanan: itemValue})}
                         >
-                            <Picker.Item label='Pilih Layanan' value='0' />
+                            {/* <Picker.Item label='Pilih Layanan' value='0' /> */}
                             <Picker.Item label="Ekonomi" value="Ekonomi" />
                         </Picker>
                     </View>
@@ -101,6 +102,10 @@ const Form = ({navigation}) => {
                             onChangeText={handleText('tanggal')}
                         /> */}
                     </View>
+
+                    <View style={Style.orang}>
+                        <Text>   Dewasa                                                   1 Orang </Text>
+                    </View>
                     <Pressable
                         style={Style.button}
                         onPress={() => navigation.navigate('Result',{data: text})}
@@ -113,27 +118,134 @@ const Form = ({navigation}) => {
     )
 }
 
-function Result({navigation}) {
-    return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>ResultPage</Text>
-        <Pressable
-          onPress={() => {
-            navigation.navigate('Form');
-          }}>
-          <Text>Back</Text>
-        </Pressable>
-      </View>
+
+
+function Result({route, navigation}) {
+    
+    const { data } = route.params;
+    console.log(data)
+
+    const check = () => {
+        let newJadwal = Jadwal.filter((item) =>{
+            return( 
+                item.asal == data.asal &&
+                item.tujuan == data.tujuan &&
+                item.layanan == data.layanan
+            )
+        })
+        return newJadwal
+    }
+
+    console.log(check())
+    const x = check()
+
+    data['jam'] = x.jam
+    data['tanggal'] = x.tanggal
+    data['harga'] = x.harga
+    
+    return (        
+        <View style={Style.container}>
+            <Text style={Style.title}>Kapalzy</Text>
+            <Text>Kuota tersedia (10000)</Text>
+            <Text>{"\n"}{"\n"}Rincian Tiket</Text>
+            <View style={Style.container2}>
+                <Text>{data.asal} -- {data.tujuan}</Text>
+                <Text>Jadwal Masuk Pelabuhan{"\n"}{data.tanggal}{"\n"}{data.jam}</Text>
+                <Text>Layanan{"\n"}{data.layanan}</Text>
+            </View>
+            <View style={Style.wrapper}> 
+                <Pressable
+                    style={Style.backbutton}
+                    onPress={() => navigation.navigate('Form')}
+                >
+                    <Text style={Style.kembali}>Kembali</Text>
+                </Pressable>
+                <Pressable
+                    style={Style.lanjutbutton}
+                    onPress={() => navigation.navigate('Detail',{data: data})}
+                >
+                    <Text style={Style.lanjutkan}>Lanjutkan</Text>
+                </Pressable>
+            </View>
+            
+            <Text>{data.harga}</Text>
+        </View>
     );
-  }
+}
+
+function Detail({route, navigation}) {
+    const { data } = route.params;
+    const [text, onChangeText] = useState({
+        nama: '',
+        identitas: '',
+        umur: '',
+    });
+    
+    const handleText = (textInput) => {
+        return (value) => {
+            onChangeText({ ...text, [textInput]: value });
+        }
+    }
+    return (        
+        <View style={Style.container}>
+            <ScrollView> 
+            <Text style={Style.title}>Kapalzy</Text>
+            <Text>{"\n"}{"\n"}Informasi Pemesanan</Text>
+            <View style={Style.container3}>
+                <Text>{data.asal} -- {data.tujuan}</Text>
+                <Text>Jadwal Masuk Pelabuhan{"\n"}{data.tanggal}{"\n"}{data.jam}</Text>
+                <Text>Layanan{"\n"}{data.layanan}</Text>
+            </View>
+            <Text>{"\n"}Data Pemesan</Text>
+            <Text style={Style.text}>Nama Lengkap</Text>
+            <View style={Style.field}>
+                <TextInput
+                    style={Style.Input}
+                    placeholder="Fadhillah Azhar Alsani"
+                    value={text.nama}
+                    onChangeText={handleText('nama')}
+                />
+            </View>
+            <Text style={Style.text}>Identitas</Text>
+            <View style={Style.field}>
+                <TextInput
+                    style={Style.Input}
+                    placeholder="Laki - laki"
+                    value={text.identitas}
+                    onChangeText={handleText('identitas')}
+                />
+            </View>
+            <Text style={Style.text}>Umur</Text>
+            <View style={Style.field}>
+                <TextInput
+                    style={Style.Input}
+                    placeholder="15 Tahun"
+                    value={text.umur}
+                    onChangeText={handleText('umur')}
+                />
+            </View>
+            
+            <Pressable
+                style={Style.button}
+                onPress={() => navigation.navigate('Result')}
+            >
+                <Text style={Style.text2}>Submit</Text>
+            </Pressable>
+            </ScrollView>
+            
+            
+        </View>
+    );
+}
 
 function Feed() {
     return (
       <Stack.Navigator
         initialRouteName="Form"
         screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Form" component={Form} />
-        <Stack.Screen name="Result" component={Result} />
+        <Stack.Screen name="Form" component={Form}/>
+        <Stack.Screen name="Result" component={Result}/>
+        <Stack.Screen name="Detail" component={Detail}/>
       </Stack.Navigator>
     );
   }
@@ -153,6 +265,17 @@ const Style = StyleSheet.create({
         width: '80%',
         marginTop: "5%",
         marginBottom: "5%",
+        left: '10%',
+        shadowColor: '#000',
+        elevation: 5,
+    },
+    container2:{
+        backgroundColor: '#F0F0F0',
+        borderRadius: 5,
+        padding: 10,
+        width: '80%',
+        marginTop: "5%",
+        marginBottom: 100,
         left: '10%',
         shadowColor: '#000',
         elevation: 5,
@@ -194,6 +317,66 @@ const Style = StyleSheet.create({
         fontSize: 20,
         padding: 5,
         color: '#fff',
+    },
+
+    orang:{
+        marginTop: 10,
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 5,
+        paddingTop: 7,
+        paddingBottom: 7,
+    },
+    backbutton:{
+        height: 40,
+        paddingHorizontal: 20,
+        marginTop: -50,
+        flex: 0,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderColor: '#00579C',
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginLeft: 50,
+    },
+
+    kembali:{
+        color: '#00579C',
+    },
+    lanjutbutton:{  
+        height: 40,
+        marginTop: -50,
+        flex: 0,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#00579C',
+        borderColor: '#00579C',
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 50,
+    },
+    lanjutkan:{
+        color: '#fff',
+    },
+    wrapper:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    container3:{
+        backgroundColor: '#F0F0F0',
+        borderRadius: 5,
+        padding: 10,
+        width: '80%',
+        marginTop: "5%",
+        left: '10%',
+        shadowColor: '#000',
+        elevation: 5,
     },
 
 })
---
+++ uts/component/db/Database.js
@@ -0,0 +1,70 @@
+const Jadwal = [
+    {
+        id: 1,
+        harga: 'Rp. 100.000',
+        asal: "Jawa",
+        tujuan: "Sumatera",
+        layanan: "Ekonomi",
+        tanggal: "Kamis, 17 Maret 2020",
+        jam: "15:30 WIB",
+    },
+    {
+        id: 2,
+        harga: 'Rp. 100.000',
+        asal: "Kalimantan",
+        tujuan: "Sulawesi",
+        layanan: "Ekonomi",
+        tanggal: "Kamis, 17 Maret 2020",
+        jam: "15:30 WIB",
+    },
+    {
+        id: 3,
+        harga: 'Rp. 100.000',
+        asal: "Bali",
+        tujuan: "Sumatera",
+        layanan: "Ekonomi",
+        tanggal: "Kamis, 17 Maret 2020",
+        jam: "15:30 WIB",
+    }
+]
+
+// const Maskapai = [
+//     {
+//         id_maskapai:"BTK",
+//         nama_maskapai:"Garuda Indonesia"
+//     },
+//     {
+//         id_maskapai:"GIA",
+//         nama_maskapai:"Garuda Indonesia"
+//     },
+//     {
+//         id_maskapai:"LNI",
+//         nama_maskapai:"Lion Air"
+//     },
+//     {
+//         id_maskapai:"SJY",
+//         nama_maskapai:"Sriwijaya Air"
+//     },
+    
+// ]
+
+// const Bandara = [
+//     {
+//         kode_bandara: "CGK",
+//         nama_bandara: "Soekarno Hatta",
+//     },
+//     {
+//         kode_bandara: "PLM",
+//         nama_bandara: "Sultan Mahmud Badaruddin II",
+//     },
+//     {
+//         kode_bandara: "HLP",
+//         nama_bandara: "Halim Perdana Kusuma",
+//     },
+//     {
+//         kode_bandara: "TKG",
+//         nama_bandara: "Radin Inten",
+//     },
+// ]
+
+export {Jadwal}
